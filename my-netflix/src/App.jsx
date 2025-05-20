4cda0e9ed5337db6819c022b6608063fcc34d346
“josy216” <josyabteka@gmail.com>
2025-05-21 02:45:08 +0300

    removed player directory

--- my-netflix/src/App.jsx
+++ my-netflix/src/App.jsx
@@ -4,7 +4,6 @@ import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 import Home from './pages/Home.jsx'
 import Login from './pages/login/Login.jsx'
-import Player from './pages/player/Player.jsx'
  import { onAuthStateChanged } from 'firebase/auth'
 import {  useEffect } from 'react'
 import { auth } from './Firebase.js' 
@@ -30,7 +29,6 @@ useEffect(() => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/player/:id' element={<Player />} />
       </Routes>
     </div>
   )
